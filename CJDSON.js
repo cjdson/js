4fd3c081919744fad28a29dfe2a7f1aacb39fb71
flip <ansuz@transitiontech.ca>
2014-12-08 12:18:07 -0500

    breaking changes: toJSON is now 'parse' to match standard js api use 'new CJDSON(cjdsonObj)' to instantiate such an object include toString method expect to be modified so as to allow 'CJDSON.parse(obj)' without 'new' keyword

---
+++ CJDSON.js
@@ -0,0 +1,46 @@
+var CJDSON={};
+
+/*
+  CJDSON refers to a format popularized in the projectmeshnet community
+  by the author of cjdns: Caleb James Delisle.
+
+  It's just JSON, but with C style comments (line and block style).
+
+  It's customary to call such a file .CJDSON, since it isn't valid JSON.
+  Your text editor likely won't recognize that file extension, though..
+  By calling any such file *.json, your editor will likely
+  highlight comments as errors. A true BUG||FEATURE situation.
+
+*/
+
+var CJDSON=function(cjdobj){
+
+  this.source=cjdobj;
+
+  this.toString=function(){
+    return this.source;
+  };
+
+  this.parse=function(){ // cjdO is text, not an object
+    return JSON.parse(this.source // Parse the results of this expression
+    .split("\n") // split by newlines to deal with line comments
+    .map(function(line,index){ // iterate over line in the resulting array
+      return line.replace(/\/\/.*$/g,""); // replace comments with empty strings
+    })
+    .join("") // join to produce a monolithic string again
+    .replace(/\/\*([\s\S]*?)\*\//g,"") // split multiline comments
+    ); // parse the results
+  }; // Enjoy your cool and refreshing JSON.
+
+  // pretty printing...
+
+  // stringify...
+
+  // ???
+
+  // profit
+
+};
+
+module.exports=CJDSON; // return this object
+// Expect more to be added to this script
